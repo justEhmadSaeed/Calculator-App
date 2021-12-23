3e7cd09e16827ad4aa9fbea215df4204a370d33c
Ehmad Saeed <justehmadsaeed@gmail.com>
2021-12-23 22:23:07 +0500

    📦 NEW: Show recent calculation history

--- src/pages/Recent.js
+++ src/pages/Recent.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 const Recent = () => {
+	const history = useSelector((state) => state.history);
+
 	return (
 		<div className="recent-page">
 			<Link to="/" className="link">
 				Back
 			</Link>
 			<h1 className="heading">Recent Calculations History</h1>
+			<div className="recent-body">
+				{history.map((calculation, key) => (
+					<div className="recent-box" key={key}>
+						<div className="recent-expression">
+							{calculation.expression}
+						</div>
+						<div className="recent-result">
+							{calculation.results}
+						</div>
+					</div>
+				))}
+			</div>
 		</div>
 	);
 };
